82ff46c4d1923431ccdf71c8d5c8050ecf858500
pkapp22 <gpkapp3@gmail.com>
2021-11-23 00:02:56 -0500

    mnove

--- CreativeCoding/BackgroundColor/sketch.js
+++ CreativeCoding/BackgroundColor/sketch.js
@@ -1,7 +1,8 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);// this creates the windo w/h to be the size of the browser window
+background(255,0,0)//red
 }
 
 function draw() {
-  background(220);
+
 }
