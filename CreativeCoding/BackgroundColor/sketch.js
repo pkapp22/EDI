d8ef3a34df89ba98b43e8c2c886f35e4cd620ae1
pkapp22 <gpkapp3@gmail.com>
2021-11-23 08:38:35 -0500

    a

--- CreativeCoding/BackgroundColor/sketch.js
+++ CreativeCoding/BackgroundColor/sketch.js
@@ -1,8 +1,31 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);// this creates the windo w/h to be the size of the browser window
-background(255,0,0)//red
+  createCanvas(720, 720);
+  fill('red')
+  strokeWeight(8);
 }
 
 function draw() {
+  background(220);
+  translate(60,60);
+  quad(0,0,
+      300, 0,
+      300,300,
+      0,300);
 
+  quad(300,0,
+      600, 0,
+      600,300,
+      300,300);
+
+  quad(0,300,
+      300, 300,
+      300,600,
+      0,600);
+
+  quad(300,300,
+      600, 300,
+      600,600,
+      300,600);
+
+  noloop();
 }
