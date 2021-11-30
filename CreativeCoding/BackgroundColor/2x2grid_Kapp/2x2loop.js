b4f3ac1adc121940b873e136bd1537bcde891776
pkapp22 <gpkapp3@gmail.com>
2021-11-30 10:29:22 -0500

    s

---
+++ CreativeCoding/BackgroundColor/2x2grid_Kapp/2x2loop.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(720, 720);
+  fill('red')
+  strokeWeight(8);
+}
+
+function draw() {
+  background(220);
+  translate(60,60);
+
+
+  for (var y=0; y < 600; y=y+300)
+
+
+
+
+
+
+  for (var x=0; x < 600; x=x+300)
+
+  quad(x,y,
+     x+ 300,y,
+     x+ 300,y+300,
+     x,y+300);
+
+
+  noloop();
+}
