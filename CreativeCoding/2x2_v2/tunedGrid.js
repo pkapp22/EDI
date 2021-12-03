d63eb8ee741a6c0fdb672a1c2b61a7effd92846e
pkapp22 <gpkapp3@gmail.com>
2021-12-03 12:34:07 -0500

    update

---
+++ CreativeCoding/2x2_v2/tunedGrid.js
@@ -0,0 +1,28 @@
+let img;
+
+function preload(){
+img = loadImage ('AIBG/Assets/Snowflake1.png')
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  fill('red')
+  strokeWeight(1);
+}
+
+function draw() {
+  image (img,0,0);
+// var num=10
+// var sideLen = windowWidth/num // variable for the side length of each square
+//
+//   for (var y=0; y < windowHeight; y=y+sideLen)
+//   for (var x=0; x < windowWidth; x=x+sideLen)
+//
+//   quad(x,y,
+//      x+ sideLen,y,
+//      x+ sideLen,y+sideLen,
+//      x,y+sideLen);
+//
+//
+//   noloop();
+// }
