c0f5cac392a7c91026a4922658e53b8545a47b3b
pkapp22 <gpkapp3@gmail.com>
2021-12-07 08:59:17 -0500

    update

--- CreativeCoding/GridImage/sketch.js
+++ CreativeCoding/GridImage/sketch.js
@@ -1,28 +1,43 @@
-let img;
+var randDrawing;
 
-function preload(){
-img = loadImage ('Assets/Snowflake1.png')
+function preload() { // all photos 150 pixel width
+
+    img = loadImage('Assets/Snowflake1.png');
+    img2 = loadImage('Assets/Snowflake2.png');
+    img3 = loadImage('Assets/Snowflake3.png');
+    img4 = loadImage('Assets/Snowflake4.png');
+    img5 = loadImage('Assets/Snowflake5.png');
+    img6 = loadImage('Assets/Snowflake6.png');
+
 }
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  fill('blue')
-  strokeWeight(1);
+function setup() { //runs once
+    createCanvas(windowWidth, windowWidth);
+    noStroke();
+    frameRate(.5);
+
 }
 
 function draw() {
-  image (img,0,0);
-var num=10
-var sideLen = windowWidth/num // variable for the side length of each square
 
-  for (var y=0; y < windowHeight; y=y+sideLen)
-  for (var x=0; x < windowWidth; x=x+sideLen)
+    var num = 12; //variable for the number of squares in the array
 
-  quad(x,y,
-     x+ sideLen,y,
-     x+ sideLen,y+sideLen,
-     x,y+sideLen);
+    var sideLen = windowWidth / num; //variable for the side length of each square
 
+    for (var y = 0; y < windowWidth; y = y + sideLen) { //loop to create rows in the y direction
 
-  noloop();
+        for (var x = 0; x < windowWidth; x = x + sideLen) { // loop to create a row of squares in the x direction
+
+            var pics = [img, img1, img2, img3, img4, img5, img6];
+
+            quad(x, y,
+                x + sideLen, y,
+                x + sideLen, y + sideLen,
+                x, y + sideLen);
+            image(pics[int(random(0, 5))], x + (sideLen / 2 - 75), y + (sideLen / 2 - 75));
+
+        }
+
+    }
+    noLoop();
 }
